cc54c5994abc8f7c0e9d0844d97465a8cccfb332
Ehsan <ehsanmousavi1.79@gmail.com>
2025-05-26 16:29:03 +0330

    [feat](assets): add icons

---
+++ src/components/icons/picture-icon.tsx
@@ -0,0 +1,27 @@
+import type { SVGProps } from "react";
+const SvgPictureIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 25 26"
+    {...props}
+  >
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M19.65 3.25H5.297c-1.133 0-2.051.97-2.051 2.167v15.166c0 1.197.918 2.167 2.05 2.167H19.65c1.132 0 2.05-.97 2.05-2.167V5.417c0-1.197-.918-2.167-2.05-2.167"
+    />
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M8.885 10.833c.85 0 1.538-.727 1.538-1.625 0-.897-.689-1.625-1.538-1.625s-1.538.728-1.538 1.625c0 .898.689 1.625 1.538 1.625M21.7 16.25l-5.126-5.417L5.297 22.75"
+    />
+  </svg>
+);
+export default SvgPictureIcon;
---
+++ src/components/icons/search-icon.tsx
@@ -0,0 +1,20 @@
+import type { SVGProps } from "react";
+const SvgSearchIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 20 20"
+    {...props}
+  >
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M9.167 15.833a6.667 6.667 0 1 0 0-13.333 6.667 6.667 0 0 0 0 13.333M17.5 17.5l-3.625-3.625"
+    />
+  </svg>
+);
+export default SvgSearchIcon;
