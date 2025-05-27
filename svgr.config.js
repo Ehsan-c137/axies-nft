b0fc2f5e76079596d54e659b55fa85235a63852c
Ehsan <ehsanmousavi1.79@gmail.com>
2025-05-27 22:58:04 +0330

    [feat]: change conifg to handle illustrations

--- svgr.config.js
+++ svgr.config.js
@@ -1,29 +1,60 @@
-module.exports = {
+const baseConfig = {
   typescript: true,
   jsxRuntime: "automatic",
-  outDir: "src/components/icons",
   dimensions: true,
-  plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx', '@svgr/plugin-prettier'],
+  plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx", "@svgr/plugin-prettier"],
   icon: true,
+  ignoreExisting: true,
+};
+
+module.exports = {
+  ...baseConfig,
+  indexTemplate: (files) => {
+    const exportEntries = files.map(({ path: filePath }) => {
+      const basename = filePath
+        .split("/")
+        .pop()
+        .replace(/\.tsx?$/, "");
+      return `export { default as ${basename} } from './${basename}'`;
+    });
+    return exportEntries.join("\n");
+  },
+  // Multiple configurations based on input path pattern
+  multipass: true,
+  ext: "tsx",
+  config: (opts) => {
+    // Check if the file is from the illustrations folder
+    if (opts.filePath.includes("illustrations")) {
+      return {
+        ...baseConfig,
+        outDir: "src/components/illustrations",
+        icon: false,
+      };
+    }
+    return {
+      ...baseConfig,
+      outDir: "src/components/icons",
+      icon: true,
+    };
+  },
+  template: ({ componentName, jsx }, { tpl }) => {
+    return tpl`
+      import type { SVGProps } from "react";
+      const ${componentName.replace("Svg", "")} = (props: SVGProps<SVGSVGElement>) => ${jsx};
+      export default ${componentName.replace("Svg", "")};
+    `;
+  },
   svgoConfig: {
     plugins: [
       {
-        name: 'preset-default',
+        name: "preset-default",
         params: {
           overrides: {
-            removeViewBox: false
-          }
-        }
-      }
-    ]
+            removeViewBox: false,
+          },
+        },
+      },
+    ],
   },
-  filenameCase: 'kebab',
-  indexTemplate: (filePaths) => {
-    const exportEntries = filePaths.map((filePath) => {
-      const basename = path.basename(filePath, path.extname(filePath))
-      const exportName = /^\d/.test(basename) ? `Svg${basename}` : basename
-      return `export { default as ${exportName} } from './${basename}'`
-    })
-    return exportEntries.join('\n')
-  }
-}
+  filenameCase: "kebab",
+};
