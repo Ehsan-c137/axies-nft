cd6a8a0a8c0d567aea291cefeb2f250b9931c31a
Ehsan <ehsanmousavi1.79@gmail.com>
2025-05-29 01:50:14 +0330

    [feat]: init swiper

---
+++ src/components/ui/swiper/swiper-item.tsx
@@ -0,0 +1,58 @@
+import clsx from "clsx";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./Swiper.module.css";
+
+type imageProp = {
+  src: string;
+  name: string;
+  slug: string;
+  itemPerPage: number;
+};
+
+export default function SwiperItem({
+  image,
+  activeIndex,
+  index,
+  itemPerPage,
+}: {
+  image: imageProp;
+  activeIndex: number;
+  index: number;
+  itemPerPage: number;
+}) {
+  return (
+    <li
+      className={styles.swiper_item}
+      style={{
+        width: `calc(100% / ${itemPerPage} )`,
+      }}
+    >
+      <Image
+        src={image.src + "?imwidth=320"}
+        alt={`Slide ${index} ${image.name}`}
+        className={clsx(styles.swiper_img, {
+          "active opacity-100": index === activeIndex,
+        })}
+        style={{
+          objectFit: "cover",
+        }}
+        draggable={false}
+        unoptimized
+        width={254}
+        height={311}
+      />
+
+      <Link href={`products/${image.slug}`}>
+        <p
+          className={clsx("text-starttruncate text-titleActive", {
+            "active opacity-100 transition translate-x-0":
+              index === activeIndex,
+          })}
+        >
+          {image.name}
+        </p>
+      </Link>
+    </li>
+  );
+}
---
+++ src/components/ui/swiper/swiper.tsx
@@ -0,0 +1,180 @@
+"use client";
+
+import React, { useState, useRef } from "react";
+import styles from "./Swiper.module.css";
+import clsx from "clsx";
+import SwiperItem from "./swiper-item";
+import { useStateRef, getRefValue } from "@/lib/hooks";
+import { getTouchEventData } from "@/lib/dom";
+import { LiveAuctionsCard } from "@/components/common/live-auctions-card-";
+
+interface IIndicatorsProps {
+  count: number;
+  activeIndex: number;
+  onSelect: (index: number) => void;
+}
+const SwiperIndicators = ({
+  count,
+  activeIndex,
+  onSelect,
+}: IIndicatorsProps) => {
+  return (
+    <div className="flex items-center gap-4 pb-2 pt-4">
+      {Array.from({ length: count })?.map((_, index) => (
+        <li
+          data-testid="indicator"
+          key={index}
+          className={clsx(styles.swiper_indicator, {
+            [styles.swiper_indicator_active]: index === activeIndex,
+          })}
+          onClick={() => onSelect(index)}
+        ></li>
+      ))}
+    </div>
+  );
+};
+
+const MIN_SWIPE_Required = 50;
+
+interface IProps {
+  images: {
+    src: string;
+    name: string;
+    slug: string;
+    id: number;
+  }[];
+  config: {
+    itemPerPage: number;
+  };
+  isLoading: boolean;
+}
+
+const Swiper = ({ images, isLoading, config }: IProps) => {
+  const { itemPerPage } = config;
+  const containerRef = useRef<HTMLUListElement>(null);
+  const containerWidthRef = useRef(0);
+  const minOffsetXRef = useRef(0);
+  const currentOffsetXRef = useRef(0);
+  const startXRef = useRef(0);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [offsetX, setOffsetX, offsetXRef] = useStateRef(0);
+  const [isSwiping, setIsSwiping] = useState(false);
+
+  const onTouchMove = (e: TouchEvent | MouseEvent) => {
+    const currentX = getTouchEventData(e).clientX;
+    const diff = getRefValue(startXRef) - currentX;
+    let newOffsetX = getRefValue(currentOffsetXRef) - diff;
+
+    const maxOffsetX = 0;
+    const minOffsetX = getRefValue(minOffsetXRef);
+
+    if (newOffsetX > maxOffsetX) {
+      newOffsetX = maxOffsetX;
+    }
+
+    if (newOffsetX < minOffsetX) {
+      newOffsetX = minOffsetX;
+    }
+
+    setOffsetX(newOffsetX);
+  };
+
+  const onTouchStart = (
+    e: React.TouchEvent<HTMLElement> | React.MouseEvent<HTMLElement>,
+  ) => {
+    setIsSwiping(true);
+
+    currentOffsetXRef.current = getRefValue(offsetXRef);
+    startXRef.current = getTouchEventData(e.nativeEvent).clientX;
+
+    const containerEl = getRefValue(containerRef);
+    const containerWidth = containerEl!.offsetWidth;
+
+    containerWidthRef.current = containerWidth;
+    minOffsetXRef.current = containerWidth - containerEl!.scrollWidth;
+
+    window.addEventListener("touchmove", onTouchMove);
+    window.addEventListener("touchend", onTouchEnd);
+    window.addEventListener("mouseup", onTouchEnd);
+    window.addEventListener("mousemove", onTouchMove);
+  };
+
+  const onTouchEnd = () => {
+    const currentOffsetX = getRefValue(currentOffsetXRef);
+    const containerWidth = getRefValue(containerWidthRef) / 4;
+    let newOffsetX = getRefValue(offsetXRef);
+
+    const diff = -newOffsetX;
+
+    if (Math.abs(diff) > MIN_SWIPE_Required) {
+      if (diff > 0) {
+        newOffsetX = Math.floor(newOffsetX / containerWidth) * containerWidth;
+      } else {
+        newOffsetX = Math.ceil(newOffsetX / containerWidth) * containerWidth;
+      }
+    } else {
+      newOffsetX = Math.round(newOffsetX / containerWidth) * containerWidth;
+    }
+
+    setIsSwiping(false);
+    setOffsetX(newOffsetX);
+    setCurrentIndex(Math.abs(newOffsetX / containerWidth));
+
+    window.removeEventListener("touchmove", onTouchMove);
+    window.removeEventListener("touchend", onTouchEnd);
+    window.removeEventListener("mouseup", onTouchEnd);
+    window.removeEventListener("mousemove", onTouchMove);
+  };
+
+  const indicatorOnClick = (index: number) => {
+    const containerEl = getRefValue(containerRef);
+    const containerWidth = containerEl!.offsetWidth;
+
+    setCurrentIndex(index);
+    setOffsetX(-(index * containerWidth));
+  };
+
+  return (
+    <div className={styles.swiper_container}>
+      <ul
+        ref={containerRef}
+        aria-label="swiper-container"
+        onTouchStart={onTouchStart}
+        onTouchEnd={onTouchEnd}
+        onMouseDown={onTouchStart}
+        onMouseUp={onTouchEnd}
+        style={{
+          transform: `translateX(${offsetX}px)`,
+        }}
+        className={clsx(styles.swiper_list, {
+          "animate-pulse bg-inputBackground": isLoading,
+          is_swiping: isSwiping,
+        })}
+      >
+        {images?.map((image, index) => {
+          return (
+            <li
+              key={index}
+              className={styles.swiper_item}
+              style={{
+                width: `calc(100% / ${itemPerPage} )`,
+                userSelect: "none",
+              }}
+            >
+              <LiveAuctionsCard key={image.id} />
+            </li>
+          );
+        })}
+      </ul>
+      <SwiperIndicators
+        count={Math.ceil(images.length / itemPerPage)}
+        activeIndex={currentIndex}
+        onSelect={indicatorOnClick}
+      />
+    </div>
+  );
+};
+
+Swiper.Indicators = SwiperIndicators;
+export default Swiper;
