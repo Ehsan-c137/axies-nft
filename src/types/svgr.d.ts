e45222c39a01620f410e097bb18d27f359a840ba
Ehsan <ehsanmousavi1.79@gmail.com>
2025-05-29 01:54:31 +0330

    [feat]: add svgr type

--- src/types/svgr.d.ts
+++ src/types/svgr.d.ts
@@ -1,10 +1,10 @@
-declare module '*.svg' {
-  import { FC, SVGProps } from 'react'
-  const content: FC<SVGProps<SVGElement>>
-  export default content
+declare module "*.svg" {
+  import * as React from "react";
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
 }
 
-declare module '*.svg?url' {
-  const content: any
-  export default content
+declare module "*.svg?url" {
+  const content: string;
+  export default content;
 }
