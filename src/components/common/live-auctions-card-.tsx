72e0dd30a2d78480ced07b7aa0279b83adb94e3a
Ehsan <ehsanmousavi1.79@gmail.com>
2025-05-29 01:56:33 +0330

    [feat]: add live-auctions & popular-collection card

---
+++ src/components/common/live-auctions-card-.tsx
@@ -0,0 +1,47 @@
+import { Image } from "@/components/ui/image";
+import HeartIcon from "@icons/heart-icon";
+
+export function LiveAuctionsCard() {
+  return (
+    <div className="flex flex-col w-full gap-3 rounded-4xl p-4 bg-card max-w-[320px]">
+      <div className="flex justify-center select-none">
+        <Image
+          className="rounded-4xl select-none shadow-md"
+          alt=""
+          src="/assets/Rectangle.png"
+          width={284}
+          height={274}
+          draggable={false}
+        />
+      </div>
+      <p>Hamlet Contemplates Yorick’s...</p>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <Image
+            src="/assets/avatar.png"
+            alt=""
+            width={44}
+            height={44}
+            draggable={false}
+          />
+          <div className="flex flex-col">
+            <p>creator</p>
+            <p>Salvador Dali</p>
+          </div>
+        </div>
+        <div className="bg-[#5142FB] rounded-full py-[1px] px-2">BSC</div>
+      </div>
+      <span className="h-[2px] bg-[var(--secondary)]"></span>
+      <div className="flex justify-between items-center">
+        <div>
+          <p>current bid</p>
+          <p>4.89 ETH</p>
+        </div>
+        <div className="flex items-center gap-1 bg-[var(--background)] px-2 py-[1px] rounded-sm cursor-pointer">
+          <HeartIcon width={11} height={11} />
+          <p className="text-xs">100</p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/common/popular-collection-card.tsx
@@ -0,0 +1,43 @@
+import { Image } from "@ui/image";
+
+export default function PopularCollectionCard() {
+  return (
+    <div
+      className="grid grid-cols-6 grid-rows-2 gap-2"
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(6, 1fr)",
+        gridTemplateRows: "repeat(2, 1fr)",
+      }}
+    >
+      {Array.from({ length: 5 }).map((image, index) => {
+        const layout = [
+          { gridColumn: "span 3 / span 2" },
+          { gridColumn: "span 3 / span 3; grid-column-start: 4" },
+          { gridColumn: "span 2 / span 2; grid-row-start: 2" },
+          {
+            gridColumn: "span 2 / span 2",
+            gridColumnStart: 3,
+            gridRowStart: 2,
+          },
+          {
+            gridColumn: "span 2 / span 2",
+            gridColumnStart: 5,
+            gridRowStart: 2,
+          },
+        ];
+        return (
+          <Image
+            src={"/assets/Rectangle.png"}
+            style={{ ...layout[index] }}
+            width={0}
+            height={0}
+            alt=""
+            sizes="100vw"
+            className="w-full h-auto"
+          />
+        );
+      })}
+    </div>
+  );
+}
