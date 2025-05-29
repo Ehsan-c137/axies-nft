1b2eb852b07205c29d5c7f19ddc2748ab3628d51
Ehsan <ehsanmousavi1.79@gmail.com>
2025-05-29 09:50:24 +0330

    [feat](assets): add icon

---
+++ src/components/icons/send-icon.tsx
@@ -0,0 +1,27 @@
+import type { SVGProps } from "react";
+const SendIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 20 20"
+    {...props}
+  >
+    <g
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      clipPath="url(#a)"
+    >
+      <path d="m18.333 1.667-9.166 9.166M18.333 1.667 12.5 18.333l-3.333-7.5-7.5-3.333z" />
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path fill="#fff" d="M0 0h20v20H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+export default SendIcon;
