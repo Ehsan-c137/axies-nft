ea732bfb32df9e4ba7be498a7ecb5116d0778d40
Ehsan <ehsanmousavi1.79@gmail.com>
2025-06-01 11:29:59 +0330

    [de]: add contained version for button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,6 +20,8 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4",
+        contained:
+          "hover:bg-[var(--card-foreground)] bg-[var(--primary)] hover:text-[var(--primary)]",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
