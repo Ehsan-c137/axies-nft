3350d8f4ea4bd38d9e9d11018f6635187a2ac261
Ehsan <ehsanmousavi1.79@gmail.com>
2025-06-03 10:34:47 +0330

    [fix]: fix typescript error

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,31 @@
 import type { NextConfig } from "next";
+import type { RuleSetRule } from "webpack";
 
 const nextConfig: NextConfig = {
- webpack(config) {    
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.('.svg'),
-    )
-    config.module.rules.push(      
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule: RuleSetRule) => {
+      if (rule && typeof rule === "object" && rule.test) {
+        if (rule.test instanceof RegExp) {
+          return rule.test.test(".svg");
+        }
+      }
+    });
+
+    config.module.rules.push(
       {
         ...fileLoaderRule,
         test: /\.svg$/i,
-        resourceQuery: /url/
-      },    
+        resourceQuery: /url/,
+      },
       {
         test: /\.svg$/i,
         issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, 
-        use: ['@svgr/webpack'],
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ["@svgr/webpack"],
       },
-    )    
-    fileLoaderRule.exclude = /\.svg$/i
-    return config
+    );
+    fileLoaderRule.exclude = /\.svg$/i;
+    return config;
   },
 };
 
