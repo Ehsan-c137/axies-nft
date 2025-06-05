fd9452e44c198bb967b5d270caa3fb18671392c0
Ehsan <ehsanmousavi1.79@gmail.com>
2025-06-05 22:24:40 +0330

    [de]: delete width & height

--- src/components/illustrations/hero-character.tsx
+++ src/components/illustrations/hero-character.tsx
@@ -2,7 +2,7 @@ type TProps = React.ComponentPropsWithoutRef<"div">;
 
 export function HeroCharacter(props: TProps) {
   return (
-    <div className={"relative w-[296px] h-[480px]"} {...props}>
+    <div className={"relative "} {...props}>
       <img
         src="/assets/hero/hero-character.png"
         alt="Hero Character"
