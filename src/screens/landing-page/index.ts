0eea582e68028d4faff611922a2ed109052d69c4
Ehsan <ehsanmousavi1.79@gmail.com>
2025-06-05 15:03:18 +0330

    delete

--- src/screens/landing-page/index.ts
+++
@@ -1,6 +0,0 @@
-export { Hero } from "./hero";
-export { LiveAuctions } from "./live-auctions";
-export { PopularCollection } from "./popular-collection";
-export { TodayPick } from "./today-pick";
-export { TopSeller } from "./top-seller";
-export { HeroOverview } from "./hero-overview";
