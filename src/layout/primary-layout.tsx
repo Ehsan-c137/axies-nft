b1756243116549740918a69d2b893f5ba08197c3
Ehsan <ehsanmousavi1.79@gmail.com>
2025-06-29 10:10:02 +0330

    [de]: add padding

--- src/layout/primary-layout.tsx
+++ src/layout/primary-layout.tsx
@@ -1,14 +1,16 @@
 import { Header } from "@/components/common/header/index";
 import { Footer } from "@/components/common/footer";
 import { HEADER_HEIGHT } from "@/lib/constant/sizes";
+import clsx from "clsx";
 
 export function PrimaryLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
       <main
+        className="md:pt-0 pt-24 px-4 md:px-0"
         style={{
-          minHeight: `calc(100vh - ${HEADER_HEIGHT}px`,
+          minHeight: `calc(100vh - ${HEADER_HEIGHT}px)`,
         }}
       >
         {children}
