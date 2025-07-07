54eccddecab4ea5c357a939b947b1f2281459765
Ehsan <ehsanmousavi1.79@gmail.com>
2025-07-07 19:32:27 +0330

    [de]: add loading state

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
+import { Spinner } from "./spinner";
 
 const buttonVariants = cva(
   "h-[46px] px-4 py-2 inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer",
@@ -24,7 +25,7 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-10 px-6 has-[>svg]:px-4 py-2",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
+        sm: "h-8 rounded-full gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-full px-6 has-[>svg]:px-4",
         icon: "w-10 h-10",
       },
@@ -40,11 +41,14 @@ function Button({
   className,
   variant,
   size,
+  loading,
   asChild = false,
+  children,
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean;
+    loading?: boolean;
   }) {
   const Comp = asChild ? Slot : "button";
 
@@ -53,7 +57,11 @@ function Button({
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
-    />
+      disabled={loading}
+    >
+      {loading && <Spinner size={"small"} />}
+      {children}
+    </Comp>
   );
 }
 
