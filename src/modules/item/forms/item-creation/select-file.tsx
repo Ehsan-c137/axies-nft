3447849269eee2c0f2cdd361ed5c469cc1aebca5
Ehsan <ehsanmousavi1.79@gmail.com>
2025-07-20 20:02:02 +0330

    [feat]: add dropdown feature

--- src/modules/item/forms/item-creation/select-file.tsx
+++ src/modules/item/forms/item-creation/select-file.tsx
@@ -11,17 +11,34 @@ type Props = {
   setValues: UseFormSetValue<TForm>;
 };
 
+const MAX_FILE_SIZE_MB = 10;
+
 export const SelectFile = ({ setValues }: Props) => {
   const ref = useRef<HTMLInputElement>(null);
+
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+  };
+  const handleDrop = (e: React.DragEvent) => {
+    e.preventDefault();
+    const files = e.dataTransfer.files[0];
+    if (!files) {
+      return;
+    }
+    setValues("image", files);
+    setValues("imageUrl", URL.createObjectURL(files));
+  };
 
   return (
     <div className="flex flex-col gap-5 w-full">
       <h4 className="text-lg font-bold">Upload file</h4>
       <Label
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
         htmlFor="post-creation-input"
         className="border h-[100px] w-full flex items-center justify-between font-light cursor-pointer md:px-8 px-4 rounded-md"
       >
-        PNG, JPG, GIF, WEBP or MP4. Max 200mb.
+        PNG, JPG, GIF, WEBP. Max 10mb.
         <Button variant="outline" onClick={() => ref.current?.click()}>
           Upload File
         </Button>
@@ -34,14 +51,23 @@ export const SelectFile = ({ setValues }: Props) => {
           multiple={false}
           onChange={(e) => {
             const file = e.target.files?.[0];
-            if (file!.size > 200 * 1024 * 1024) {
-              toast.error("File size exceeds 200MB limit.");
+            if (!file) {
               return;
             }
-            if (file) {
-              setValues("image", file);
-              setValues("imageUrl", URL.createObjectURL(file));
+
+            const maxFileSizeInBytes = MAX_FILE_SIZE_MB * 1024 * 1024;
+            if (file.size > maxFileSizeInBytes) {
+              toast.error("File size exceeds 10MB limit.");
+              if (ref.current) {
+                setValues("image", null);
+                setValues("imageUrl", "");
+                ref.current.value = "";
+              }
+              return;
             }
+
+            setValues("image", file);
+            setValues("imageUrl", URL.createObjectURL(file));
           }}
         />
       </Label>
