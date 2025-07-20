422c0ad03930301ade808e5804cb4f540f2bfe7d
Ehsan <ehsanmousavi1.79@gmail.com>
2025-07-21 00:10:19 +0330

    [feat]: add blog service

--- src/services/blog/blog-service.ts
+++ src/services/blog/blog-service.ts
@@ -1,18 +1,48 @@
 import { useQuery } from "@tanstack/react-query";
 import { BASE_URL } from "../config";
 
-async function getBlogPosts(page: number = 1, limit: number = 8) {
-  const response = fetch(`${BASE_URL}/blog?page=${page}&limit=${limit}`);
+export const BLOG_QUERY = {
+  allBlogs: () => ["blog-posts"],
+  blogDetail: (id: number) => ({
+    queryKey: [...BLOG_QUERY.allBlogs(), id],
+    queryFn: () => getBlogDetail(id),
+    staleTime: 5 * 1000,
+  }),
+};
 
+export async function getBlogPosts(page: number = 1, limit: number = 8) {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/blog?page=${page}&limit=${limit}`,
+    );
+
+    if (!response.ok) {
+      console.log("something went wrong");
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error("Error fetching blog posts:", error);
+    throw new Error("Failed to fetch blog posts");
+  }
+}
+
+async function getBlogDetail(id: number) {
+  const response = fetch(`${BASE_URL}/blog/${id}`);
   return (await response).json();
 }
 
 export function useBlogPosts(page = 1, ...args: any) {
   return useQuery({
-    queryKey: ["blog-posts", page],
+    queryKey: [...BLOG_QUERY.allBlogs(), page],
     queryFn: () => getBlogPosts(page),
     placeholderData: (previousData) => previousData,
+    ...args,
   });
+}
+
+export function useBlogDetail(id: number, ...args: any) {
+  return useQuery(BLOG_QUERY.blogDetail(id));
 }
 
 const blogPosts = {
