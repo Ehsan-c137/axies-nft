1f1196264a1e80d4410e00fc86df7e1a447bbdf8
Ehsan <ehsanmousavi1.79@gmail.com>
2025-07-27 21:12:30 +0330

    [feat]: handle paginatin range for extra pages

--- src/components/common/pagination-control.tsx
+++ src/components/common/pagination-control.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import {
   Pagination,
   PaginationContent,
@@ -7,13 +8,71 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@ui/pagination";
+import { useSearchParams } from "next/navigation";
 
 interface IProps {
   currentPage: number;
   lastPage: number;
+  url?: string;
 }
 
-export default function PaginationControl({ currentPage, lastPage }: IProps) {
+const range = (start: number, end: number) => {
+  const length = end - start + 1;
+  return Array.from({ length }, (_, idx) => idx + start);
+};
+const DOTS = "...";
+
+export default function PaginationControl({
+  currentPage,
+  lastPage,
+  url,
+}: IProps) {
+  const searchParams = useSearchParams();
+
+  let activePage = currentPage;
+  if (!url) {
+    activePage = Number(searchParams.get("page"));
+  }
+
+  const paginationRange = useMemo(() => {
+    const siblingCount = 1;
+    // totalPageNumbers: siblingCount + firstPage + lastPage + currentPage + 2*DOTS
+    const totalPageNumbers = siblingCount + 5;
+
+    if (totalPageNumbers >= lastPage) {
+      return range(1, lastPage);
+    }
+
+    const leftSiblingIndex = Math.max(activePage - siblingCount, 1);
+    const rightSiblingIndex = Math.min(activePage + siblingCount, lastPage);
+
+    const shouldShowLeftDots = leftSiblingIndex > 2;
+    const shouldShowRightDots = rightSiblingIndex < lastPage - 2;
+
+    if (!shouldShowLeftDots && shouldShowRightDots) {
+      const leftItemCount = 3 + 2 * siblingCount;
+      const leftRange = range(1, leftItemCount);
+      return [...leftRange, DOTS, lastPage];
+    }
+
+    if (shouldShowLeftDots && !shouldShowRightDots) {
+      const rightItemCount = 3 + 2 * siblingCount;
+      const rightRange = range(lastPage - rightItemCount + 1, lastPage);
+      return [1, DOTS, ...rightRange];
+    }
+
+    if (shouldShowLeftDots && shouldShowRightDots) {
+      const middleRange = range(leftSiblingIndex, rightSiblingIndex);
+      return [1, DOTS, ...middleRange, DOTS, lastPage];
+    }
+
+    return range(1, lastPage);
+  }, [activePage, lastPage]);
+
+  if (lastPage <= 1) {
+    return null;
+  }
+
   return (
     <>
       {lastPage > 1 && (
@@ -22,31 +81,46 @@ export default function PaginationControl({ currentPage, lastPage }: IProps) {
             <PaginationContent>
               <PaginationItem>
                 <PaginationPrevious
-                  href={`?page=${currentPage - 1}`}
-                  aria-disabled={currentPage === 1}
+                  href={
+                    url
+                      ? `/${url}/${activePage - 1}`
+                      : `?page=${activePage - 1}`
+                  }
+                  aria-disabled={activePage === 1}
                   className={
-                    currentPage < 1 ? "pointer-events-none opacity-50" : ""
+                    activePage <= 1 ? "pointer-events-none opacity-50" : ""
                   }
                 />
               </PaginationItem>
-              {Array.from({ length: lastPage }, (_, index) => index + 1).map(
-                (page) => (
+              {paginationRange?.map((page, index) => {
+                if (page === DOTS) {
+                  return (
+                    <PaginationItem key={`${page}-${index}`}>
+                      <PaginationEllipsis />
+                    </PaginationItem>
+                  );
+                }
+                return (
                   <PaginationItem key={page}>
                     <PaginationLink
-                      href={`?page=${page}`}
-                      isActive={page === currentPage}
+                      href={url ? `/${url}/${page}` : `?page=${page}`}
+                      isActive={page === activePage}
                     >
                       {page}
                     </PaginationLink>
                   </PaginationItem>
-                ),
-              )}
+                );
+              })}
               <PaginationItem>
                 <PaginationNext
-                  href={`?page=${currentPage + 1}`}
-                  aria-disabled={currentPage >= lastPage}
+                  href={
+                    url
+                      ? `/${url}/${activePage + 1}`
+                      : `?page=${activePage + 1}`
+                  }
+                  aria-disabled={activePage >= lastPage}
                   className={
-                    currentPage >= lastPage
+                    activePage >= lastPage
                       ? "pointer-events-none opacity-50"
                       : ""
                   }
