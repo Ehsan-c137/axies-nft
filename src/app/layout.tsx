822ab85396202d79357031553c60e8848c7e0e0a
Ehsan <ehsanmousavi1.79@gmail.com>
2025-07-27 21:07:50 +0330

    [feat]: make layout async

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
   description: "A marketplace for NFTs",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
