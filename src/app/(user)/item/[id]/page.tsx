88a04e627d93e6ea59ac16184a628af93ed92d4c
Ehsan <ehsanmousavi1.79@gmail.com>
2025-07-27 21:02:54 +0330

    [feat]: add GenereateStaticParams & fix meta data

--- src/app/(user)/item/[id]/page.tsx
+++ src/app/(user)/item/[id]/page.tsx
@@ -1,22 +1,37 @@
 import type { Metadata } from "next";
 import ItemDetailScreen from "@/screens/user/item-detail/item-screen";
+import {
+  getAllItems,
+  getItemDetail,
+  TItem,
+} from "@/services/item/item-service";
 
 type Props = {
   params: Promise<{ id: string }>;
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
-// export async function generateMetadata({ params }: Props): Promise<Metadata> {
-//   const id = params.id;
-//   const item = await fetch(`https://api.example.com/items/${id}`).then(
-//     (res) => res.json(),
-//   );
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = await params;
+  const item = await getItemDetail(id);
 
-//   return {
-//     title: item.name,
-//     description: item.description,
-//   };
-// }
+  if (!item) {
+    throw new Error("item not found");
+  }
+
+  return {
+    title: item.name,
+    description: item.description,
+  };
+}
+
+export async function generateStaticParams() {
+  const items: TItem[] = await getAllItems();
+
+  return items.map((item) => ({
+    id: String(item.id),
+  }));
+}
 
 export default async function Page({
   params,
@@ -24,5 +39,12 @@ export default async function Page({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  return <ItemDetailScreen id={id} />;
+
+  const item = await getItemDetail(id);
+
+  if (!item) {
+    throw new Error("Item not found");
+  }
+
+  return <ItemDetailScreen id={id} initialData={item} />;
 }
