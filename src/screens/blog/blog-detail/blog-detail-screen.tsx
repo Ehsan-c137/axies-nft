0cc47191d1edd189e7b381dee95279cb46508fff
Ehsan <ehsanmousavi1.79@gmail.com>
2025-07-27 22:18:57 +0330

    [feat]: add some content

--- src/screens/blog/blog-detail/blog-detail-screen.tsx
+++ src/screens/blog/blog-detail/blog-detail-screen.tsx
@@ -1,3 +1,12 @@
-export default function BlogDetailScreen({ slug }) {
-  return <section className="container mx-auto px-4 py-10">asf</section>;
+interface IProps {
+  blog: any;
+}
+
+export default function BlogDetailScreen({ blog }: IProps) {
+  return (
+    <section className="container mx-auto px-4 py-10">
+      <p>not implemented yet</p>
+      <p className="text-gray-600 mb-4">{blog?.content}</p>
+    </section>
+  );
 }
