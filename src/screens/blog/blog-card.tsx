da191a6f07d4166d30651990c15691b3897db1c6
Ehsan <ehsanmousavi1.79@gmail.com>
2025-07-28 23:12:17 +0330

    [feat]: add props

--- src/screens/blog/blog-card.tsx
+++ src/screens/blog/blog-card.tsx
@@ -5,38 +5,57 @@ import { useRouter } from "next/navigation";
 import { Avatar, AvatarFallback, AvatarImage } from "@ui/avatar";
 import { Button } from "@ui/button";
 
-export function BlogCard() {
+interface IProps {
+  slug: string;
+  title: string;
+  authorName: string;
+  date: string;
+  thumbnail: string;
+  authorImage: string;
+  description: string;
+}
+
+export function BlogCard({
+  title,
+  authorName,
+  date,
+  thumbnail,
+  description,
+  slug,
+  authorImage,
+}: IProps) {
   const router = useRouter();
   return (
     <div className="p-4 grid gap-4 max-w-[450px] bg-[var(--card)] text-[var(--card-foreground)] rounded-2xl not-dark:shadow-sm">
       <div className="w-full h-[280px] relative">
         <Image
-          src="/assets/blog-image.jpg"
-          alt=""
+          src={thumbnail}
+          alt={title}
           className="object-cover absolute left-0 top-0 w-full h-full rounded-2xl"
           width={0}
           height={0}
-          objectFit="cover"
           unoptimized
         />
       </div>
       <div className="flex items-center">
         <div className="flex w-full gap-4">
           <Avatar className="w-11 h-11 rounded-2xl">
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
+            <AvatarImage src={authorImage} />
+            <AvatarFallback>
+              {authorName?.charAt(0).toUpperCase()}
+            </AvatarFallback>
           </Avatar>
           <div className="flex flex-1 items-center justify-between w-full">
-            <h3 className="text-sm font-bold">John Doe</h3>
-            <p className="text-sm text-muted-foreground">Feb 19</p>
+            <h3 className="text-sm font-bold">{title}</h3>
+            <p className="text-sm text-muted-foreground">{date}</p>
           </div>
         </div>
       </div>
-      <p className="line-clamp-2">
-        Dolore officia sint incididunt non excepteur ea mollit commodo ut enim
-        reprehenderit cupidatat labore ad laborum consectetur consequat
-      </p>
-      <Button variant="outline" onClick={() => router.push("/blog/asdf")}>
+      <p className="line-clamp-2">{description}</p>
+      <Button
+        variant="outline"
+        onClick={() => router.push(`/blog/detail/${slug}`)}
+      >
         Read More
       </Button>
     </div>
