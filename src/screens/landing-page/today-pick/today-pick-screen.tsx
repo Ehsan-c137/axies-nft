be2b00f8a3acc675699da001b82ef2c67fcba7e2
Ehsan <ehsanmousavi1.79@gmail.com>
2025-07-28 21:28:27 +0330

    [refactor]: change file name & add PaginationLoadmore

---
+++ src/screens/landing-page/today-pick/today-pick-screen.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { Button } from "@ui/button";
+import { LiveAuctionsCard } from "@common/cards/live-auctions-card";
+import { PaginationLoadMore } from "@common/pagination/pagination-load-more";
+import Filter from "./filter";
+import { useGetTodayPick } from "@/services/item/today-pick";
+import useSearchParamState from "@/hooks/useSearchParamState";
+import { useEffect, useState } from "react";
+import { TItem } from "@/services/item/item-service";
+
+interface IProps {
+  ref: React.RefObject<HTMLElement[]>;
+}
+
+const PARAM_KEYS = {
+  page: "today_pick_page",
+  limit: "today_pick_limit",
+  category: "today_pick_category",
+  collections: "today_pick_collections",
+};
+
+export function TodayPick({ ref }: IProps) {
+  const [showedData, setShowedData] = useState<TItem[]>([]);
+
+  const { handleParamChange, optimisticParams: paramState } =
+    useSearchParamState();
+
+  const { data, isPending, isPlaceholderData, error, isError } =
+    useGetTodayPick({
+      page: Number(paramState[PARAM_KEYS.page]) || 1,
+      limit: Number(paramState[PARAM_KEYS.limit]) || 8,
+      category: paramState[PARAM_KEYS.category],
+      collection: paramState[PARAM_KEYS.collections],
+    });
+
+  const currentPage = data?.meta?.currentPage;
+  const lastPage = data?.meta?.lastPage;
+
+  const handleLoadMore = () => {
+    handleParamChange(PARAM_KEYS.page, String(currentPage + 1), true, true);
+
+    if (data?.meta && data.meta.currentPage < data.meta.lastpage) {
+      setShowedData((prevData) => [...prevData, ...data?.data]);
+    }
+  };
+
+  useEffect(() => {
+    if (data?.data && !isPlaceholderData) {
+      if (data.meta.currentPage === 1) {
+        setShowedData(data.data);
+      } else {
+        setShowedData((prevData) => [...prevData, ...data.data]);
+      }
+    }
+  }, [lastPage, currentPage]);
+
+  console.log({ showedData, data, paramState });
+
+  return (
+    <>
+      <div
+        className="flex flex-col gap-10 py-10 opacity-0"
+        ref={(el) => {
+          if (el) {
+            ref.current[4] = el;
+          }
+        }}
+      >
+        <div className="flex items-center justify-between">
+          <h3 className="text-xl lg:text-3xl font-bold">Today&apos;s Pick</h3>
+          <Button variant="link" className="explore_more text-sm lg:text-xl">
+            EXPLORE MORE
+          </Button>
+        </div>
+        <Filter paramState={paramState} handleParamChange={handleParamChange} />
+        <PaginationLoadMore
+          data={showedData}
+          DataCard={LiveAuctionsCard}
+          isPending={isPending}
+          handleLoadMore={handleLoadMore}
+          isPlaceholderData={isPlaceholderData}
+          lastPage={data?.meta?.lastPage || 1}
+          currentPage={data?.meta?.currentPage || 1}
+          error={error}
+          isError={isError}
+        />
+      </div>
+    </>
+  );
+}
