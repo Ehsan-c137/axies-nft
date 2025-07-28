2ea24cd9b0f598dfa7a2441a9bcf61a2a1f458c2
Ehsan <ehsanmousavi1.79@gmail.com>
2025-07-28 23:19:50 +0330

    [refactor]: add initial data and fix typo

--- src/screens/blog/blogs-screen.tsx
+++ src/screens/blog/blogs-screen.tsx
@@ -1,26 +1,35 @@
 "use client";
 
-import PaginationList from "@/components/common/pagination-list";
+import PaginationList from "@/components/common/pagination/pagination-list";
 import { BlogCard } from "./blog-card";
 import CardPlaceholder from "@/components/common/cards/card-placeholder";
 import { useBlogPosts } from "@/services/blog/blog-service";
+import { TPaginatedData } from "@/types/service";
 
-export default function BlogScreen() {
+interface IProps {
+  blogs: any;
+  currentPage: number;
+}
+
+export default function BlogScreen({ blogs, currentPage }: IProps) {
   const {
     data: blogPosts,
     isPending,
     error,
     isPlaceholderData,
-  } = useBlogPosts();
+  } = useBlogPosts(currentPage, {
+    initialData: blogs,
+  });
 
   return (
     <PaginationList
       error={error}
       isPending={isPending}
-      paginatedData={blogPosts}
+      paginatedData={blogPosts as TPaginatedData}
       DataCard={BlogCard}
       PlaceholderCard={CardPlaceholder}
       isPlaceholderData={isPlaceholderData}
+      url={"blog"}
     />
   );
 }
