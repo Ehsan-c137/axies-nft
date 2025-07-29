8333d629288545da1915b01709a63423b5c1c791
Ehsan <ehsanmousavi1.79@gmail.com>
2025-07-30 00:54:21 +0330

    [feat]: init

---
+++ src/components/ui/swiper/swiper-indicators.tsx
@@ -0,0 +1,31 @@
+import styles from "./swiper.module.css";
+import clsx from "clsx";
+
+interface IIndicatorsProps {
+  count: number;
+  activeIndex: number;
+  onSelect: (index: number) => void;
+}
+export const SwiperIndicators = ({
+  count,
+  activeIndex,
+  onSelect,
+}: IIndicatorsProps) => {
+  if (count <= 1) {
+    return null;
+  }
+  return (
+    <ul className="flex items-center gap-4 pb-2 pt-4">
+      {Array.from({ length: count })?.map((_, index) => (
+        <li
+          data-testid="indicator"
+          key={index}
+          className={clsx(styles.swiper_indicator, {
+            [styles.swiper_indicator_active]: index === activeIndex,
+          })}
+          onClick={() => onSelect(index)}
+        ></li>
+      ))}
+    </ul>
+  );
+};
