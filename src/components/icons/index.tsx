6808a4df41973a60aa3575a5ed868223228cc27c
Ehsan <ehsanmousavi1.79@gmail.com>
2025-07-30 00:52:12 +0330

    [feat]: add icons

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,7 +1,7 @@
 export { default as Bookmark } from "./bookmark";
+export { default as BoxIcon } from "./box-icon";
 export { default as EllipsisIcon } from "./ellipsis-icon";
 export { default as EyeIcon } from "./eye-icon";
-export { default as FileIcon } from "./file-icon";
 export { default as FireIcon } from "./fire-icon";
 export { default as HeartIcon } from "./heart-icon";
 export { default as MoonIcon } from "./moon-icon";
@@ -10,6 +10,7 @@ export { default as PictureIcon } from "./picture-icon";
 export { default as RefreshIcon } from "./refresh-icon";
 export { default as RocketIcon } from "./rocket-icon";
 export { default as SearchIcon } from "./search-icon";
+export { default as SendHorizontalIcon } from "./send-horizontal-icon";
 export { default as SendIcon } from "./send-icon";
 export { default as ShareIcon } from "./share-icon";
 export { default as ShoppingBagIcon } from "./shopping-bag-icon";
--- src/components/icons/socials/facebook-icon.tsx
+++ src/components/icons/socials/facebook-icon.tsx
@@ -2,24 +2,20 @@ import type { SVGProps } from "react";
 const FacebookIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
     width="1em"
     height="1em"
-    fill="none"
-    viewBox="0 0 20 20"
+    fill="#fff"
     stroke="currentColor"
+    viewBox="0 0 512 512"
     {...props}
   >
-    <g clipPath="url(#a)">
-      <path
-        fill="#fff"
-        d="M19.688 10A9.686 9.686 0 0 0 10 .313 9.686 9.686 0 0 0 .313 10c0 4.835 3.542 8.843 8.173 9.57V12.8h-2.46V10h2.46V7.866c0-2.428 1.446-3.77 3.66-3.77 1.06 0 2.168.19 2.168.19v2.383h-1.222c-1.203 0-1.578.747-1.578 1.513V10H14.2l-.43 2.8h-2.256v6.77c4.63-.727 8.174-4.735 8.174-9.57"
-      />
-    </g>
-    <defs>
-      <clipPath id="a">
-        <path fill="#fff" d="M0 0h20v20H0z" />
-      </clipPath>
-    </defs>
+    <path
+      d="M283.122 122.174v46.583h83.424l-9.045 74.367h-74.379v268.375h-98.726V243.124h-51.443v-74.367h51.443v-56.302c0-27.82-2.096-41.02 9.725-62.578C205.948 28.32 239.308-.174 297.007.512c57.713.711 82.04 6.263 82.04 6.263l-12.501 79.257s-36.853-9.731-54.942-6.263c-18.065 3.469-28.482 14.597-28.482 42.405z"
+      style={{
+        display: "inline",
+      }}
+    />
   </svg>
 );
 export default FacebookIcon;
