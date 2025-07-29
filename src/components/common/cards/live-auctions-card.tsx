988ca1c39d2455e6c955d6f5f37d66e93ad23131
Ehsan <ehsanmousavi1.79@gmail.com>
2025-07-30 00:52:35 +0330

    [feat]: add props

--- src/components/common/cards/live-auctions-card.tsx
+++ src/components/common/cards/live-auctions-card.tsx
@@ -1,64 +1,74 @@
 import Image from "next/image";
-import HeartIcon from "@icons/heart-icon";
-import FireIcon from "@icons/fire-icon";
+import RefreshIcon from "@icons/refresh-icon";
 import ShoppingBagIcon from "@icons/shopping-bag-icon";
 import { Button } from "@ui/button";
 import Link from "next/link";
+import { TItem } from "@/services/item/item-service";
 
-export function LiveAuctionsCard() {
+export function LiveAuctionsCard({
+  name,
+  image,
+  price,
+  createdBy,
+  platform,
+  slug,
+}: TItem) {
   return (
-    <div className="flex flex-col w-full gap-3 rounded-4xl p-4 bg-[var(--card)] text-[var(--card-foreground)] max-w-[320px] animate-card-hover hover:-translate-y-2 transition ease-in-out duration-[350ms] not-dark:shadow-sm">
+    <div className="flex flex-col w-full gap-3 rounded-4xl p-4 bg-[var(--card)] text-[var(--card-foreground)] max-w-[320px] hover:-translate-y-2 transition ease-in-out duration-[350ms] not-dark:shadow-sm">
       <div className="flex justify-center select-none relative group">
-        <Image
-          className="rounded-4xl select-none shadow-md"
-          alt=""
-          src="/assets/Rectangle.png"
-          width={284}
-          height={274}
-          draggable={false}
-        />
-        <Link
-          href={"item/123"}
+        <div className="w-[284px] h-[274px] rounded-4xl overflow-hidden select-none not-dark:shadow-sm relative">
+          <Image
+            className="select-none absolute w-full h-full object-cover"
+            alt={`${name} image`}
+            src={image}
+            width={284}
+            height={274}
+            unoptimized
+            draggable={false}
+          />
+        </div>
+        <Button
           className="absolute flex gap-1 bottom-5 z-1 group-hover:-translate-y-20 opacity-0 group-hover:opacity-100 transition-all duration-300"
+          variant="contained"
         >
-          <Button variant="contained">
-            <ShoppingBagIcon className="hover:fill-[#5142FB]" /> Place a bid
-          </Button>
-        </Link>
-        <span className="flex items-center gap-2 absolute bottom-5 z-1 bg-[var(--card)] px-3 py-2 rounded-xl">
-          <FireIcon />
-          <p className="font-bold text-sm">5 : 23 : 22 : 08</p>
-        </span>
+          <ShoppingBagIcon className="hover:fill-[#5142FB]" /> Place a bid
+        </Button>
       </div>
-      <p>Hamlet Contemplates Yorick’s...</p>
+      <p>{name}</p>
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-2">
           <Image
-            src="/assets/avatar.png"
-            alt=""
+            src={createdBy?.image ?? "/avatar_1.png"}
+            alt={""}
             width={44}
             height={44}
+            unoptimized
             draggable={false}
+            className="rounded-full"
           />
           <div className="flex flex-col">
-            <p>creator</p>
-            <p>Salvador Dali</p>
+            <p className="font-bold">creator</p>
+            <Link href={`/profile/${createdBy?.username}`}>
+              {createdBy?.name}
+            </Link>
           </div>
         </div>
-        <div className="bg-[var(--primary)] text-white rounded-full py-[1px] px-2">
-          BSC
+        <div className="bg-[var(--primary)] rounded-full py-[1px] px-2">
+          {platform}
         </div>
       </div>
       <span className="h-[2px] bg-[var(--secondary)]"></span>
       <div className="flex justify-between items-center">
         <div>
-          <p>current bid</p>
-          <p>4.89 ETH</p>
+          <p className="font-bold">current bid</p>
+          <p>{price}</p>
         </div>
-        <div className="flex items-center gap-1 bg-[var(--background)] rounded-[8px] p-2 cursor-pointer">
-          <HeartIcon width={18} height={18} />
-          <p className="text-xs">100</p>
-        </div>
+        <Link
+          href={`item/${slug}`}
+          className="flex items-center gap-2 bg-[var(--background)] rounded-xl p-2 cursor-pointer"
+        >
+          <RefreshIcon /> view history
+        </Link>
       </div>
     </div>
   );
