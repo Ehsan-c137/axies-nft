79227137fa1d3c5a1ceda5a8a3769f7a6fe6c6b4
Ehsan <ehsanmousavi1.79@gmail.com>
2025-07-30 21:14:59 +0330

    [refactor]: use countdown hook instead of timer

--- src/modules/item/countdown-card.tsx
+++ src/modules/item/countdown-card.tsx
@@ -1,18 +1,17 @@
-import useTimer from "@/hooks/useTimer";
+import { useCountdown } from "@/hooks/use-countdown";
 
 interface IProps {
-  countdown: string;
+  countdown: number;
 }
 
 export default function Countdown({ countdown }: IProps) {
-  const { days, hours, minutes, seconds, message } = useTimer(countdown);
-  const isExpired = message?.toLocaleLowerCase() === "expired";
+  const { days, hours, minutes, seconds, hasEnded } = useCountdown(countdown);
 
   return (
     <div className="flex justify-items-center items-center justify-center lg:justify-between flex-1 bg-[var(--card)] text-[var(--card-foreground)] rounded-2xl px-6 py-2 h-[60px] md:h-[50px] not-dark:shadow-sm">
       <p className="font-semibold text-sm leading-1">Countdown</p>
       <div
-        className={`flex font-bold leading-1 ${isExpired ? "opacity-50" : "opacity-100"}`}
+        className={`flex font-bold leading-1 ${hasEnded ? "opacity-50" : "opacity-100"}`}
         tabIndex={0}
         role="timer"
       >
