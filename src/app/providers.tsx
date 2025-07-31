da870c98df9c18c2d70c4c4d4caf5e74d129f852
Ehsan <ehsanmousavi1.79@gmail.com>
2025-07-31 18:48:48 +0330

    [feat]: add gsap

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,10 +4,13 @@ import { useState } from "react";
 import { ThemeProvider } from "@/context/theme/theme-context";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { Toaster } from "@ui/sonner";
-
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 interface IProps {
   children: React.ReactNode;
 }
+
+gsap.registerPlugin(useGSAP);
 
 export default function Providers({ children }: IProps) {
   const [queryClient] = useState(
