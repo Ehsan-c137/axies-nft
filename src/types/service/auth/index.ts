32247b81348f068bcd9d40c36f257f09f6f023d9
Ehsan <ehsanmousavi1.79@gmail.com>
2025-08-02 19:47:16 +0330

    [feat]: init

---
+++ src/types/service/auth/index.ts
@@ -0,0 +1,12 @@
+export interface ILoginCredentials {
+  email: string;
+  password: string;
+}
+
+export interface IUser {
+  id: string;
+  name: string;
+  email: string;
+  image: string;
+  role: string;
+}
