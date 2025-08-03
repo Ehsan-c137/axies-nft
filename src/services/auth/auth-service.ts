fd6529dd23640ce3571b7c40aeda70f58dfe01d6
Ehsan <ehsanmousavi1.79@gmail.com>
2025-08-03 15:15:11 +0330

    [feat]: add login & logout mutation

--- src/services/auth/auth-service.ts
+++ src/services/auth/auth-service.ts
@@ -3,16 +3,44 @@ import { PROFILE_QUERY } from "../users/user-service";
 import { useQueryClient } from "@tanstack/react-query";
 import { useAuth } from "@/context/auth/auth-provider";
 
-export function useLogoutMutation(id: number) {
+export function useLogoutMutation() {
   const { logout } = useAuth();
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: logout,
     onSuccess: () => {
-      queryClient.invalidateQueries(PROFILE_QUERY.userProfile(id));
+      queryClient.clear();
     },
     onError: (error: Error) => {
       console.error("Logout error:", error);
+    },
+  });
+}
+
+export function useLoginMutation() {
+  const { login } = useAuth();
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: login,
+    onSuccess: (user) => {
+      queryClient.setQueryData([...PROFILE_QUERY.user(), user?.id], user);
+    },
+    onError: (error: Error) => {
+      console.error("Login error:", error);
+    },
+  });
+}
+
+export function useSignupMutation() {
+  const { signup } = useAuth();
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: signup,
+    onSuccess: (user) => {
+      queryClient.setQueryData([...PROFILE_QUERY.user(), user?.id], user);
+    },
+    onError: (error: Error) => {
+      console.error("Signup error:", error);
     },
   });
 }
