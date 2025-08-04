64df36d322f645d2579d0935b72cb10d4ae4495b
Ehsan <ehsanmousavi1.79@gmail.com>
2025-08-04 20:53:10 +0330

    [feat]: add back button

--- src/components/common/errors/404.tsx
+++ src/components/common/errors/404.tsx
@@ -1,13 +1,24 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 export default function NotFound() {
+  const router = useRouter();
   return (
     <div className="flex flex-col items-center justify-center gap-4 min-h-[400px] text-center">
       <h2 className="text-4xl font-bold uppercase ">Not Found</h2>
       <p>Could not find requested resource</p>
-      <Link href="/" className="underline">
-        Return to home
-      </Link>
+      <div className="flex flex-col items-center gap-4">
+        <Button variant="outline" onClick={() => router.back()}>
+          Back
+        </Button>
+        or
+        <Button variant="outline" onClick={() => router.push("/")}>
+          Return to home
+        </Button>
+      </div>
     </div>
   );
 }
