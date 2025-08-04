4bfda229b38e8866efaba656f8e80934cdbd3ed4
Ehsan <ehsanmousavi1.79@gmail.com>
2025-08-04 22:24:22 +0330

    [feat]: add api

--- src/screens/user/profile/profile-screen.tsx
+++ src/screens/user/profile/profile-screen.tsx
@@ -7,6 +7,11 @@ import { Tabs } from "./tabs";
 import { toast } from "sonner";
 import { useCopyToClipboard } from "@/hooks/useCopyToClipboard";
 import { useRouter } from "next/navigation";
+import { useAuth } from "@/context/auth/auth-provider";
+import { useUserProfile } from "@/services/users/user-service";
+import UnexpectedError from "@/components/common/errors/unexpected";
+import { ProfileScreenPlaceholder } from "./profile-screen-placeholder";
+import NotFoundContainer from "@/components/common/errors/404";
 
 type Props = {
   username: string;
@@ -14,33 +19,43 @@ type Props = {
 
 export function ProfileScreen({ username }: Props) {
   const router = useRouter();
-  const isMe = username === "me";
+  const { user } = useAuth();
+  const isMe = user?.username === username;
+
+  const { data, isError, isLoading, error } = useUserProfile(username);
+  console.log({ isMe, data }, user?.username);
+  if (isLoading) {
+    return <ProfileScreenPlaceholder />;
+  }
+
+  if (isError) {
+    return <UnexpectedError error={error} />;
+  }
+
+  if (!data || data?.status == 404) {
+    return <NotFoundContainer />;
+  }
 
   return (
     <div className="container mx-auto">
       <div className="flex flex-wrap justify-between items-center rounded-t-3xl p-4 md:p-9 bg-[url(/assets/bg-profile.webp)] bg-opacity-60 bg-no-repeat bg-cover md:max-h-[274px]">
-        <div className="flex flex-wrap md:flex-nowrap md:-translate-y-[26px] gap-8 items-center">
+        <div className="flex flex-wrap md:flex-nowrap gap-1 md:gap-8 items-center">
           <div className="relative md:h-[270px] min-w-[200px] min-h-[200px] w-full h-full">
-            <Avatar className="w-full h-full absolute rounded-3xl md:translate-y-6">
-              <AvatarImage
-                src="/assets/images-item-details.jpg"
-                className="object-cover"
-              />
-              <AvatarFallback>CN</AvatarFallback>
+            <Avatar className="w-full h-full absolute rounded-3xl">
+              <AvatarImage src={data?.image} className="object-cover" />
+              <AvatarFallback>{data?.name[0]}</AvatarFallback>
             </Avatar>
           </div>
-          <div className="flex flex-col items-start gap-4 ">
-            <h2 className="text-lg md:text-4xl text-white">
-              Mehdi Khanmohammadi
-            </h2>
+          <div className="flex flex-col items-start gap-1 md:gap-4 ">
+            <h2 className="text-lg md:text-4xl text-white">{data?.name}</h2>
             <p>@{username}</p>
             <p className="max-w-lg text-xs leading-6 text-white">
               Exprience next generation of bots with us.
             </p>
-            <ShareAddress />
+            <ShareAddress address={data?.walletAddress} />
           </div>
         </div>
-        <div className="h-full flex pt-8">
+        <div className="h-full flex justify-end md:justify-start pt-8">
           {isMe ? (
             <Button
               variant="contained"
@@ -58,7 +73,7 @@ export function ProfileScreen({ username }: Props) {
   );
 }
 
-const ShareAddress = () => {
+const ShareAddress = ({ address }: { address: string }) => {
   const { copy } = useCopyToClipboard();
 
   const handleShare = () => {
@@ -69,7 +84,7 @@ const ShareAddress = () => {
 
   return (
     <Button variant="outline" onClick={handleShare}>
-      DdzFFzCqrhshMSxABCdfrge <FileIcon />
+      {address} <FileIcon />
     </Button>
   );
 };
