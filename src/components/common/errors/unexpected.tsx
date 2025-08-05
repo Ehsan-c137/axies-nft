6022e37a894ef0ea3e59477a1e60553899bda8ae
Ehsan <ehsanmousavi1.79@gmail.com>
2025-08-05 12:42:07 +0330

    [feat]: add navigation button

--- src/components/common/errors/unexpected.tsx
+++ src/components/common/errors/unexpected.tsx
@@ -1,14 +1,30 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
 interface IProps {
   error: Error;
 }
 
 export default function UnexpectedError({ error }: IProps) {
-  let errorMessage;
-  if (error instanceof Error) {
-    errorMessage = <p>Error: {error?.message}</p>;
-  } else {
-    errorMessage = <p>something went wrong</p>;
-  }
+  const router = useRouter();
+  console.log(error);
 
-  return <div className="w-full flex justify-center">{errorMessage}</div>;
+  return (
+    <div className="w-full flex flex-col justify-center items-center gap-4">
+      <p>Ooops! something went wrong.</p>
+      <div className="flex flex-col gap-2">
+        <Button variant="outline" onClick={() => router.refresh()}>
+          Try agian
+        </Button>
+        <Button variant="outline" onClick={() => router.back()}>
+          Back
+        </Button>
+        <Button variant="outline" onClick={() => router.push("/")}>
+          Home
+        </Button>
+      </div>
+    </div>
+  );
 }
