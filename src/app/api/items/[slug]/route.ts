ab614c3e7296bb8bb8620adf17d9d29ac0098a20
Ehsan <ehsanmousavi1.79@gmail.com>
2025-08-05 23:53:04 +0330

    [feat]: add status code

--- src/app/api/items/[slug]/route.ts
+++ src/app/api/items/[slug]/route.ts
@@ -9,7 +9,10 @@ export async function GET(
     const post = allProducts.find((p) => p.slug === postSlug);
 
     if (!post) {
-      return Response.json({ message: "Item not found", status: 404 });
+      return Response.json(
+        { message: "Item not found", status: 404 },
+        { status: 404 },
+      );
     }
 
     return Response.json(post);
@@ -17,6 +20,9 @@ export async function GET(
     const message =
       error instanceof Error ? error.message : "An unexpected error occurred";
     console.error("Unexpected error:", error);
-    return Response.json({ message, error: true, status: 500 });
+    return Response.json(
+      { message, error: true, status: 500 },
+      { status: 500 },
+    );
   }
 }
