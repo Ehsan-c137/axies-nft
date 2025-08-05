bd4bbb527d5a21b9593d966b1bc047dc6f91b33a
Ehsan <ehsanmousavi1.79@gmail.com>
2025-08-05 12:41:55 +0330

    [de]: change text size

--- src/components/common/errors/404.tsx
+++ src/components/common/errors/404.tsx
@@ -8,15 +8,14 @@ export default function NotFound() {
   const router = useRouter();
   return (
     <div className="flex flex-col items-center justify-center gap-4 min-h-[400px] text-center">
-      <h2 className="text-4xl font-bold uppercase ">Not Found</h2>
+      <h2 className="text-3xl font-bold uppercase ">Not Found | 404</h2>
       <p>Could not find requested resource</p>
-      <div className="flex flex-col items-center gap-4">
+      <div className="flex items-center gap-4">
         <Button variant="outline" onClick={() => router.back()}>
           Back
         </Button>
-        or
         <Button variant="outline" onClick={() => router.push("/")}>
-          Return to home
+          home
         </Button>
       </div>
     </div>
