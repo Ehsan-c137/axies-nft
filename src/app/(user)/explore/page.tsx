005b82a8a3d5203f2ec519fcaaf704c43ef9592c
Ehsan <ehsanmousavi1.79@gmail.com>
2025-08-16 14:42:38 +0330

    [feat]: add force-dynamic

--- src/app/(user)/explore/page.tsx
+++ src/app/(user)/explore/page.tsx
@@ -1,6 +1,8 @@
 import ExploreScreen from "@/screens/user/explore/explore-screen";
 import { Suspense } from "react";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return (
     <Suspense>
