822843e508ff9102866174294cdf7df0f25ff1f3
Ehsan <ehsanmousavi1.79@gmail.com>
2025-08-16 15:02:00 +0330

    [refactor]: add data  file url

--- src/services/server/items.ts
+++ src/services/server/items.ts
@@ -1,9 +1,27 @@
-import { BASE_URL } from "../config";
 import { logger } from "@/utils/logger";
+
+const URL =
+  "https://raw.githubusercontent.com/Ehsan-c137/axies-nft/main/src/mocks/items.json";
 
 export async function getItemDetail(slug: string) {
   try {
-    const response = await fetch(`${BASE_URL}/items/${slug}`);
+    const response = await fetch(URL);
+    const allProducts = await response.json();
+
+    const itemDetail = allProducts.find(
+      (item: { slug: string }) => item.slug === slug,
+    );
+
+    return itemDetail;
+  } catch (error) {
+    logger.log(error);
+    return null;
+  }
+}
+
+export async function getAllItems() {
+  try {
+    const response = await fetch(URL);
     return response.json();
   } catch (error) {
     logger.log(error);
