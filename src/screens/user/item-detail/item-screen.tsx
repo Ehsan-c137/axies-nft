7743772c74c5716d815c0f51dc8f871dbca8a9b5
Ehsan <ehsanmousavi1.79@gmail.com>
2025-08-16 14:57:32 +0330

    [refactor]: add default image size

--- src/screens/user/item-detail/item-screen.tsx
+++ src/screens/user/item-detail/item-screen.tsx
@@ -56,8 +56,8 @@ export default function ItemDetailScreen({
             unoptimized
             src={itemDetail?.image}
             alt={`${itemDetail.name} image`}
-            width="0"
-            height="0"
+            width="300"
+            height="300"
             sizes="100vw"
             className="w-full h-auto rounded-xl"
           />
