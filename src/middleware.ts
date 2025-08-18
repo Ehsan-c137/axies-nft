94e26fc8a9c731a3e5f9375b8764145f3ece7ecd
Ehsan <ehsanmousavi1.79@gmail.com>
2025-08-18 16:03:21 +0330

    [refactor]: delete unessesry comment

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,9 +7,6 @@ export function middleware(request: NextRequest) {
   if (!sessionRefreshToken) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
-
-  // If the refresh token exists, we can assume the user has an active session.
-  // The client-side is responsible for managing the access token (fetching a new one if needed).
   return NextResponse.next();
 }
 
