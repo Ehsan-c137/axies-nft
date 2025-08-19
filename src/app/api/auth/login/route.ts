255e4fcbfa5d4d401c67385e9c131b0db700a12b
Ehsan <ehsanmousavi1.79@gmail.com>
2025-08-20 00:56:00 +0330

    [refactor]: change cookie attrubutes

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -4,9 +4,6 @@ import { NextResponse } from "next/server";
 import { REFRESH_TOKEN } from "@/services/config";
 
 export async function POST(request: Request) {
-  const isCI = process.env.CI === "true";
-  const isProduction = process.env.APP_ENV === "production";
-
   try {
     const credentials: ILoginCredentials = await request.json();
 
@@ -14,13 +11,6 @@ export async function POST(request: Request) {
       credentials.email === MOCK_USER.email &&
       credentials.password === "p@ssword123"
     ) {
-      let sameSite: "strict" | "lax" | "none" = "strict";
-      if (isCI) {
-        sameSite = "none";
-      } else if (!isProduction) {
-        sameSite = "lax";
-      }
-
       const response = NextResponse.json(
         {
           accessToken: MOCK_ACCESS_TOKEN,
@@ -33,10 +23,9 @@ export async function POST(request: Request) {
         name: REFRESH_TOKEN,
         value: MOCK_REFRESH_TOKEN,
         httpOnly: true,
-        path: "/api/auth",
-        domain: isCI ? "localhost" : undefined,
-        secure: isProduction,
-        sameSite: sameSite,
+        path: "/",
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
         maxAge: 60 * 60 * 24 * 7,
       });
 
