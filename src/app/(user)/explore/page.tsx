0fd2d1edda2f37cccaf17f5e47fb8be58315bccc
Ehsan <ehsanmousavi1.79@gmail.com>
2025-08-20 19:50:10 +0330

    [feat]: add metadata

--- src/app/(user)/explore/page.tsx
+++ src/app/(user)/explore/page.tsx
@@ -1,7 +1,13 @@
 import ExploreScreen from "@/screens/user/explore/explore-screen";
 import { Suspense } from "react";
+import type { Metadata } from "next";
 
 export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "Explore",
+  description: "explore nft items",
+};
 
 export default function Page() {
   return (
